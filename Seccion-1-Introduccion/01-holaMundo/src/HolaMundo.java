3de76fd2b022e139c4dbf7ea76e1578eeee7b124
TomasRivetta <totorivetta@gmail.com>
2024-10-15 20:03:12 -0300

    seccion 01 finalizada

---
+++ Seccion-1-Introduccion/01-holaMundo/src/HolaMundo.java
@@ -0,0 +1,7 @@
+public class HolaMundo {
+
+    // PUBLICO, STATICO , VOID(PORQUE NO RETIRNA NINGUN VALOR), recibe parametros de tipo arreglo de tipo string
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
---
+++ Seccion-1-Introduccion/01-holaMundo/src/tarea01.java
@@ -0,0 +1,28 @@
+public class tarea01 {
+
+    //Crear una clase java e imprimir tu nombre en la salida o consola.
+    /*
+    * El objetivo de esta primera tarea es crear una nueva clase con el método main y dentro imprime tu nombre en la salida estándar o consola utilizando System.out.println
+
+Aunque parece simple, es importante desde el principio conocer bien la clase main y la salida estándar de java en consola.
+
+
+
+IMPORTANTE: Como nota aparte todas las tareas publicadas en el curso son opcionales,
+no afecta en nada en el avance del curso ni en el temario, tampoco afecta en la entrega
+final del diploma o reconocimiento por haber finalizado el curso, son 100% opcionales y
+todas traen incluidas el código de solución.
+Así es como funciona el sistema de tareas de udemy, las tareas no quedan marcadas con
+checked (como realizadas), esta funcionalidad no tiene nada que ver con nuestro curso
+ni el instructor, así es como funciona la plataforma de Udemy.
+
+
+Preguntas de esta tarea
+Publica y envía acá mismo tu código de la tarea, solo la clase con el main, pero también es importante que lo pruebes localmente en tu equipo en el IDE Intellij o el que estés utilizando.
+    * */
+
+    public static void main(String[] args) {
+        System.out.println("Tomas");
+    }
+
+}
